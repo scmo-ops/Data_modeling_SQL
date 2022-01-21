99c7ea1e95c70ece62b9cce69535f1d8be940509
scmo-ops <a01610125@exatec.tec.mx>
2022-01-21 17:48:51 -0600

    7 FASTER

--- EmployeeSQL/Employee.sql
+++ EmployeeSQL/Employee.sql
@@ -43,4 +43,12 @@ WHERE departments.dept_name = 'Sales';
 
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+INNER JOIN departments ON (dept_emp.dept_no = departments.depto_no)
+INNER JOIN employees ON ( dept_emp.emp_no = employees.emp_no)
+WHERE departments.dept_name = 'Sales' OR  departments.dept_name = 'Development';
+
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT COUNT()
