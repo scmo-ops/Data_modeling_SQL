30818c9c6e45886746c5b2ae2d5a2d703a3da85e
scmo-ops <a01610125@exatec.tec.mx>
2022-01-21 17:45:26 -0600

    6 done
    fast

--- EmployeeSQL/Employee.sql
+++ EmployeeSQL/Employee.sql
@@ -35,6 +35,11 @@ WHERE first_name = 'Hercules' AND (last_name like 'B%');
 
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+INNER JOIN departments ON (dept_emp.dept_no = departments.depto_no)
+INNER JOIN employees ON ( dept_emp.emp_no = employees.emp_no)
+WHERE departments.dept_name = 'Sales';
 
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
