690e247d2ce9aa192fccff37a29b1b5838f977b9
scmo-ops <a01610125@exatec.tec.mx>
2022-01-21 17:42:26 -0600

    4 and 5 done
    ;V

--- EmployeeSQL/Employee.sql
+++ EmployeeSQL/Employee.sql
@@ -22,10 +22,19 @@ INNER JOIN employees ON (employees.emp_no = dept_manager.emp_no);
 
 --4. List the department of each employee with the following information: employee number, last name, first name, and department name.
 
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+INNER JOIN departments ON (dept_emp.dept_no = departments.depto_no)
+INNER JOIN employees ON ( dept_emp.emp_no = employees.emp_no);
 
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules' AND (last_name like 'B%');
+
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
 
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
