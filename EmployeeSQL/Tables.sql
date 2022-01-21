38360f37c8d56894a62ff1d3424d135d77939802
scmo-ops <a01610125@exatec.tec.mx>
2022-01-21 15:55:36 -0600

    Final Tables
    Yey

--- EmployeeSQL/Tables.sql
+++ EmployeeSQL/Tables.sql
@@ -7,27 +7,16 @@ PRIMARY KEY (depto_no)
 );
 --SELECT * FROM departments
 
---Department employeee table(2)
-DROP TABLE if exists dept_emp;
-CREATE TABLE dept_emp(
-emp_no int,
-dept_no VARCHAR(30),
-PRIMARY KEY (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments(depto_no)
+--Title table (2)
+DROP TABLE if exists titles;
+CREATE TABLE titles (
+title_id VARCHAR(30) NOT NULL,
+title VARCHAR(30) NOT NULL,
+PRIMARY KEY (title_id)
 );
---SELECT * FROM dept_emp
+--SELECT * FROM titles
 
---Department manager(3)
-DROP TABLE if exists dept_manager;
-CREATE TABLE dept_manager(
-dept_no VARCHAR(30),
-emp_no int,
-FOREIGN KEY (dept_no) REFERENCES departments(depto_no),
-FOREIGN KEY (emp_no) REFERENCES dept_emp(emp_no)
-);
---SELECT * FROM dept_manager
-
--- Table of all Employees (4)
+-- Table of all Employees (3)
 DROP TABLE if exists employees;
 CREATE TABLE employees(
 emp_no int,
@@ -37,26 +26,40 @@ first_name VARCHAR(30),
 last_name VARCHAR(30),
 sex VARCHAR(1),
 hire_date date,
-
+PRIMARY KEY(emp_no),
+FOREIGN KEY(emp_title_id) REFERENCES titles(title_id)
 );
---SELECT * FROM employees
+SELECT * FROM employees
 
---Salary table (5)
+--Salary table (4)
 DROP TABLE if exists salaries;
 CREATE TABLE salaries(
 emp_no int,
 salary int,
-PRIMARY KEY (salary),
-FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+PRIMARY KEY (emp_no)
 );
 --SELECT * FROM salaries
 
---Title table (6)
-DROP TABLE if exists titles;
-CREATE TABLE titles (
-title_id VARCHAR(30),
-title VARCHAR(30),
-PRIMARY KEY (title_id)
+--Department employeee table(5)
+DROP TABLE if exists dept_emp;
+CREATE TABLE dept_emp(
+emp_no int,
+dept_no VARCHAR(30),
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments(depto_no),
+PRIMARY KEY (emp_no, dept_no)
 );
+--SELECT * FROM dept_emp
 
---SELECT * FROM titles
+--Department manager(6)
+DROP TABLE if exists dept_manager;
+CREATE TABLE dept_manager(
+dept_no VARCHAR(30),
+emp_no int,
+FOREIGN KEY (dept_no) REFERENCES departments(depto_no),
+FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+PRIMARY KEY (dept_no, emp_no)
+);
+--SELECT * FROM dept_manager
+
