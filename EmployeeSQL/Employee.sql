d349e71f305d566bf2ce14332ca42d09279ed240
scmo-ops <a01610125@exatec.tec.mx>
2022-01-21 17:21:42 -0600

    Second and third solved
    yey

--- EmployeeSQL/Employee.sql
+++ EmployeeSQL/Employee.sql
@@ -4,18 +4,24 @@
 
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees
-LEFT OUTER JOIN salaries
+INNER JOIN salaries
 ON employees.emp_no = salaries.salary;
 
 --2. List first name, last name, and hire date for employees who were hired in 1986.
 
 SELECT first_name, last_name, hire_date 
 FROM employees
-WHERE hire_date  986%";
+WHERE hire_date  BETWEEN '1986-01-01' AND '1986-12-31';
 
 --3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM dept_manager
+INNER JOIN departments ON (dept_manager.dept_no = departments.depto_no)
+INNER JOIN employees ON (employees.emp_no = dept_manager.emp_no);
+
 --4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
 
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
