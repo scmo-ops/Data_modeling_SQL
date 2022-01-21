49ef252101b32327df6eb2e06de0d503ead6e3a5
scmo-ops <a01610125@exatec.tec.mx>
2022-01-21 17:52:33 -0600

    And done

--- EmployeeSQL/Employee.sql
+++ EmployeeSQL/Employee.sql
@@ -51,4 +51,7 @@ WHERE departments.dept_name = 'Sales' OR  departments.dept_name = 'Development';
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
-SELECT COUNT()
+SELECT last_name, COUNT(last_name) as LN_Freq
+FROM employees
+GROUP BY last_name
+ORDER BY LN_Freq desc
